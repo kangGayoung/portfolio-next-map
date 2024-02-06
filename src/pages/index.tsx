8971dab12f55b54e1abcba290bc4bb3960a134be
kangGayoung <gangga9011@gmail.com>
2024-02-06 16:53:23 +0900

    마이페이지 컴포넌트 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,12 +21,22 @@ export default function Home({ stores }: { stores: StoreType[] }) {
     );
 }
 
-export async function getStaticProps() {
+// export async function getStaticProps() {
+//     // api/stores get 요청
+//     const stores = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`);
+//     // ).then((res) => res.json()); // respose를 json으로 변경
+//     return {
+//         props: { stores: stores.data },
+//         revalidate: 60 * 60, //**한시간마다 한번 update**
+//     };
+// }
+
+// fetching 할때마다  새로운 데이터 변경
+export async function getServerSideProps() {
     // api/stores get 요청
     const stores = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`);
     // ).then((res) => res.json()); // respose를 json으로 변경
     return {
         props: { stores: stores.data },
-        revalidate: 60 * 60, //한시간마다
     };
 }
